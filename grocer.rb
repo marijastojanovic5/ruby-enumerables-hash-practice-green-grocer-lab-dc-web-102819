e2aad36a604c9e8012d9d423034dbace6a4dcc1d
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 02:31:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ coupons.each do |coupon|
    if cart.include?(name) && cart[name][:count] >= num_of_c
        new_cart[name][:count] -= num_of_c
        if new_cart["#{name} W/COUPON"]
-         new_cart["#{name} W/COUPON"][:count] +=1
+         new_cart["#{name} W/COUPON"][:count] *2
          else
          new_cart["#{name} W/COUPON"] = {
            :price => coupon[:cost],
