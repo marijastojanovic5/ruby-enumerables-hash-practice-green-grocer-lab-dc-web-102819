7670eaf86c8954d366770a80eec3cacdb38dc568
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 03:35:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,8 +18,7 @@ coupons.each do |coupon|
   item=coupon[:item]
   if cart.has_key?(item)
     if cart[item][:count]>=coupon[:num] &&  !cart.has_key? ("#{item} W/COUPON")
-       cart["#{item} W/COUPON"]={ price: coupon[:cost] /coupon[:num],clearance: cart[item][:clearance]
-    ,count: coupon[:num]}
+       cart["#{item} W/COUPON"]={ price: coupon[:cost] /coupon[:num],clearance: cart[item][:clearance],count: coupon[:num]}
     cart[item][:count] -= coupon[:num]
     elsif  cart[item][:count] >= coupon[:num] && cart.has_key? ("#{item} W/COUPON")
        cart["#{item} W/COUPON"][:count] +=coupon[:num]
