668315cac14159846cd473fe29bb9815a066a17f
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 03:13:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,8 +17,8 @@ def apply_coupons(cart, coupons)
 coupons.each do |coupon|
   item=coupon[:item]
   if cart[item]
-    if cart[item][:count]>=coupon[:num] && !cart.has_key"#{item} W/COUPON"]
-       cart
+    if cart[item][:count]>=coupon[:num] && !cart.has_key("#{item} W/COUPON")
+       cart{"#{item}"
     
  
    
