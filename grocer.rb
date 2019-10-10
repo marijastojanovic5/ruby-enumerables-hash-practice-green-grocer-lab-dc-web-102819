298d52bfc80fe19446f7fa85ca8f945362c2dd88
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 01:49:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ def apply_coupons(cart, coupons)
     #if the cart has the same item in coupon and has larger amount than in coupon
     if cart.include?(name) && cart[name][:count] >= num_of_c
        #remove number of the new_cart's count
-       new_cart[name][:count] -= num_of_c
+       new_cart[name][:count] += num_of_c
        #increase the count when there is more items than the coupon allows
        if new_cart["#{name} W/COUPON"]
          new_cart["#{name} W/COUPON"][:count] += 1
