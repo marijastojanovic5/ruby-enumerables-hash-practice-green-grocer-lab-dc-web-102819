edc32fc9ada5710dfbbce419e817a1818352ad72
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 01:38:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,56 +14,43 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
-  new_cart = {}
-  cart.each do |grocery, info|
-    coupons.each do |coupon|
-      if grocery == coupon[:item] && info[:count] >= coupon[:num]
-        cart[grocery][:count] = cart[grocery][:count] - coupon[:num]
-        if new_cart[grocery + " W/COUPON"]
-          new_cart[grocery + " W/COUPON"][:count] += 1
-        else
-          new_cart[grocery + " W/COUPON"] = {:price => coupon[:cost], :clearance => cart[grocery][:clearance], :count => 1}
-        end
-      end
-    end
-    new_cart[grocery] = info
+  #doesn't break if there is no coupon
+  return cart if coupons == []
 
-  end
-  new_cart
+  #set new_cart = cart so we don't have to push all the values, just change them
+  new_cart = cart
+
+  coupons.each do |coupon|
+    name = coupon[:item] #avocado, cheese,...
+    num_of_c = coupon[:num]
+    #if the cart has the same item in coupon and has larger amount than in coupon
+    if cart.include?(name) && cart[name][:count] >= num_of_c
+       #remove number of the new_cart's count
+       new_cart[name][:count] -= num_of_c
+       #increase the count when there is more items than the coupon allows
+       if new_cart["#{name} W/COUPON"]
+         new_cart["#{name} W/COUPON"][:count] += 1
+       #set the name with coupon with new value
+       else
+         new_cart["#{name} W/COUPON"] = {
+           :price => coupon[:cost],
+           :clearance => new_cart[name][:clearance],
+           :count => 1
+         }
+       end
+     end
+   end
+   new_cart
 end
 
-def apply_clearance(cart)
-  # code here
-  clearance_cart = {}
-  cart.each do |item, info|
-    clearance_cart[item] = {}
-    info.each do |datum|
-      if cart[item][:clearance]
-        clearance_cart[item][:price] = (cart[item][:price] * 0.80).round(2)
-      else
-        clearance_cart[item][:price] = cart[item][:price]
-      end
-      clearance_cart[item][:clearance] = cart[item][:clearance]
-      clearance_cart[item][:count] = cart[item][:count]
+#result:
+{
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+}
 
-    end
-  end
-  clearance_cart
-end
+# apply clearance:
+# takes 20% off price if the item is on clearance
+# does not discount the price for items not on clearance
 
-def checkout(cart: [], coupons: [])
- cart = consolidate_cart(cart:cart)
-  cart = apply_coupons(cart:cart, coupons:coupons)
-  cart = apply_clearance(cart:cart)
-
-  total = 0
-  cart.each do |item, properties|
-    total += properties[:price] * properties[:count]
-  end
-
-  total > 100 ? total -= total * 0.1 : nil
-
-  total
-end 
- 
