c3f4d71c26888d791dc84a08e9764438ed3e04ca
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 02:11:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,29 +13,7 @@ def consolidate_cart(cart)
   updated_cart
 end
 
-def apply_coupons(cart:[], coupons:[])
-  	    new_hash = {}
-    cart.each do |vegetable, properties|
-      if !new_hash[vegetable]
-        new_hash[vegetable] = properties
-      end
-      coupons.each do |hash|
-        hash.each do |coupon_key, coupon_value|
-          if coupon_value == vegetable
-            if !new_hash["#{vegetable} W/COUPON"]
-              new_hash["#{vegetable} W/COUPON"] = {:price => hash[:cost], :clearance => cart[vegetable][:clearance], :count => 0}
-            end
-            if !(new_hash[vegetable][:count] < hash[:num])
-              new_hash[vegetable][:count] -= hash[:num]
-              new_hash["#{vegetable} W/COUPON"][:count] += 1
-            end
-          end
-        end
-      end
-    end
-    new_hash
 
-end
 
 #result:
 {
