8b4c51d5dfcc4411f0fce88963a530d59cf483cb
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 01:17:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,24 +50,3 @@ def apply_clearance(cart:[])
 end	
 end
 
-
-	def checkout(cart: [], coupons: [])
-  cart = consolidate_cart(cart:cart)
-  cart = apply_coupons(cart:cart, coupons:coupons)
-  cart = apply_clearance(cart:cart)
-
-  total = 0
-  cart.each do |item, properties|
-    total += properties[:price] * properties[:count]
-  end
-
-  total > 100 ? total -= total * 0.1 : nil
-
-  total
-end 
-end
-
-
-
-
-
