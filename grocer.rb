c07448e74c96a6112853efe0284c2aa9138db239
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-10 01:10:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,16 @@
 def consolidate_cart(cart)
- 
+  updated_cart = {}
+  cart.each do |item|
+    item.each do |k, v|
+      if !updated_cart[k]
+        updated_cart[k] = v
+        updated_cart[k][:count] = 1
+      else
+        updated_cart[k][:count] += 1
+      end
+    end
+  end
+  updated_cart
 end
 
 def apply_coupons(cart, coupons)
